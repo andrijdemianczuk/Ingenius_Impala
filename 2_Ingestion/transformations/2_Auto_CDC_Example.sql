1162a0f29593f5a7fe1d2efeab7f8cda7f018c6c
andrijdemianczuk <andrijdemianczuk>
2025-09-05 16:31:02 +0000

    Fixed execution and naming

--- 2_Ingestion/transformations/2_Auto_CDC_Example.sql
+++ 2_Ingestion/transformations/2_Auto_CDC_Example.sql
@@ -1,11 +1,10 @@
-CREATE OR REFRESH STREAMING TABLE ademianczuk.ncr.s_forecast
+CREATE OR REFRESH STREAMING TABLE ademianczuk.ncr.s_updated_wind
 TBLPROPERTIES ("quality" = "silver");
 
 CREATE FLOW flow -- A 'flow' is a description of a pipeline. We use this to 'declare' what we want to happen in a pipeline.
 AS AUTO CDC INTO -- What we want to happen, and where it's going into.
-  ademianczuk.ncr.s_forecast
+  ademianczuk.ncr.s_updated_wind
 FROM STREAM(ademianczuk.ncr.b_wind_data)
-  KEYS (FORECAST_DATE_LOCAL) -- The key refers to our 'merge' identifier. There can be several keys chained together. This can be whatever identifies a row in the target table as 'unique'
+  KEYS (FORECAST_DATE_LOCAL) -- The key refers to our 'merge' identifier. There can be several keys chained together.
   SEQUENCE BY FORECAST_DATE_LOCAL -- This is a required field. It defines the 'order' or sequence to which the data is being ingested.
---   COLUMNS * EXCEPT (FORECAST_DATE_LOCAL) -- This is a list of columns to update. We default to *.
   STORED AS SCD TYPE 1; -- Whether or not to handle updates as UPSERTs or append a new row with a start/end time for the old values.
